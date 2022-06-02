1f1c33e8d4102eb15c7fc460de7a10af324b8852
Felipe Pardo <felipepardo90@hotmail.com>
2022-06-01 23:41:34 -0300

    calculadora

---
+++ index.js
@@ -0,0 +1,32 @@
+let display = document.getElementById("display");
+
+let buttons = Array.from(document.getElementsByClassName("button"));
+
+console.log(buttons);
+
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    // console.log("clicked");
+    // console.log(e)
+    // console.log(e.target.innerText)
+    switch (e.target.innerText) {
+      case "C":
+        display.innerText = "";
+        break;
+      case "←":
+        if (display.innerText) {
+          display.innerText = display.innerText.slice(0, -1);
+          break;
+        }
+      case "=":
+        try {
+          display.innerText = eval(display.innerText);
+        } catch (error) {
+          display.innerText = "Error!";
+        }
+        break;
+      default:
+        display.innerText += e.target.innerText;
+    }
+  });
+});
