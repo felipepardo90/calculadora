ca7352de68dc840fa4942f6379a4da8a8797b565
Felipe Pardo <felipepardo90@hotmail.com>
2023-01-29 20:48:41 -0300

    new styles and fonts

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 let display = document.getElementById("display");
-
 let buttons = Array.from(document.getElementsByClassName("button"));
 
-console.log(buttons);
 
 buttons.map((button) => {
   button.addEventListener("click", (e) => {
-    // console.log("clicked");
-    // console.log(e)
-    // console.log(e.target.innerText)
     switch (e.target.innerText) {
       case "C":
         display.innerText = "";
